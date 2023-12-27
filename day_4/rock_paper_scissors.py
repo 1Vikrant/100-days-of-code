b6e9e15b57d91d471554c095eee010387d39d0ef
Vikrant Mishra <48333224+1Vikrant@users.noreply.github.com>
2023-12-27 19:40:03 +0530

    Fix day4 project

--- day_4/rock_paper_scissors.py
+++ day_4/rock_paper_scissors.py
@@ -1,64 +1,73 @@
 import random
 
-# Play the game
-options = [0,0,0]
+rock =  '''
+              ROCK
+             ______
+        ____/   ___)_
+                _____)
+                _____)
+        _______ ____)
+               \___)'''
 
-options[0] =  '''
-              _______
-          ___/   ____)
-                 _____)
-                 _____)
-                 _____)
-             ---.____)'''
-
-options[1] =  '''
-              ___________
-          ___/   ________)_
-                   ________)
-                 __________)
-                 _________)
-             ---.________)'''
+paper =  '''
+                PAPER
+             ____________
+        ____/   _________)_
+                ___________)
+                ___________)
+        _______ ___________)
+               \_________)'''
              
-options[2] = '''
+scissor = '''
+               SCISSOR
               _______
-          ___/   ____)____
-                   _______)
-                 _________)
-                 _____)
-             ---.____)'''
+        _____/  _____)_____
+                ___________)
+                ___________)
+        _______ _______)
+               \_____)'''
 
+player_choices = [0, 1, 2]
+options = [rock, paper, scissor]
 
 playing = 'y'
 
-while playing == 'y':
+while playing.lower() == 'y':
+    #player's turn
+    invalid = True
+    while invalid:
+        player_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper, 2 for Scissors.\n"))
+        if player_choice in player_choices:
+            invalid = False
+        else:
+            print("Please enter a valid choice[0/1/2]")
 
-    # Get the choices
-    player_chose = int(input("What do you choose? Type 0 for Rock, 1 for Paper, 2 for Scissors.\n"))
-    computer_chose = random.randint(0, 2)
+    # Computer's Turn
+    computer_choice = random.randint(0, 2)
 
     print("You chose: ")
-    print(options[player_chose])
+    print(options[player_choice])
     print("computer chose: ")
-    print(options[computer_chose])
+    print(options[computer_choice])
 
-    if player_chose == computer_chose:
+    if player_choice == computer_choice:
         print("Drawn!")
     else:
-        if player_chose == 0:
-            if computer_chose == 1:
-                print(f"Result : You lose!")
-            elif computer_chose == 2:
+        if player_choice == 0:
+            if computer_choice == 1:
+                print("Result : You lose!")
+            elif computer_choice == 2:
                 print("Result: You Win!")
-        if player_chose == 1:
-            if computer_chose == 0:
+        if player_choice == 1:
+            if computer_choice == 0:
                 print("Result: You Win")
-            elif computer_chose == 2:
+            elif computer_choice == 2:
                 print("Result: You lose")
-        if player_chose == 2:
-            if computer_chose == 0:
+        if player_choice == 2:
+            if computer_choice == 0:
                 print("You lose")
-            elif computer_chose == 1:
+            elif computer_choice == 1:
                 print("You Win.")
-    playing = input("enter 'y' to play more")
+    playing = input("enter 'y' to play more: ")
 
 print("Thanks for visiting")
